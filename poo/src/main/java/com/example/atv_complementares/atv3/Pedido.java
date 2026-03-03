9df5688f61b8ecc24fdc7249784ff1301f5ffa10
michellfloat <michell.ssfloat@gmail.com>
2026-03-03 16:24:13 -0300

    EXTRA: Need to fix these errors of complementary activity

--- poo/src/main/java/com/example/atv_complementares/atv3/Pedido.java
+++ poo/src/main/java/com/example/atv_complementares/atv3/Pedido.java
@@ -10,20 +10,45 @@ public class Pedido {
     Scanner teclado = new Scanner(System.in);
     ArrayList<String> listaDeProdutos = new ArrayList<>();
 
-    listaDeProdutos.add("espada");
-    listaDeProdutos.add("")
+    //Produto 1
+    Produto produto1 = new Produto();
+    produto1.setNome("Espada");
+    produto1.setPreco(375.87);
+    produto1.setQuantEstoque(67);
 
-    Produto produto = new Produto();
+    //Produto 2
+    Produto produto2 = new Produto();
+    produto2.setNome("Machado");
+    produto2.setPreco(650.35);
+    produto2.setQuantEstoque(12);
+
+    //Produto 3
+    Produto produto3 = new Produto();
+    produto3.setNome("Arco");
+    produto3.setPreco(525.50);
+    produto3.setQuantEstoque(0);
+
+    //Adicionando os produtos na Array:
+    listaDeProdutos.add(produto1.getNome());
+    listaDeProdutos.add(produto2.getNome());
+    listaDeProdutos.add(produto3.getNome());
+
+    
 
     System.out.println("Lojas AMADO!!");
     System.out.println("Digite o produto que deseja comprar:");
-    System.out.println("1-espada->Quantidade atual:"+produto.getQuantEstoque());
-    System.out.println("2-Machado->Quantidade atual:"+produto.getQuantEstoque());
-    System.out.println("3-Arco->Quantidade atual:"+produto.getQuantEstoque());
+    System.out.println("1-Espada->Quantidade atual:"+produto1.getQuantEstoque());
+    System.out.println("2-Machado->Quantidade atual:"+produto2.getQuantEstoque());
+    System.out.println("3-Arco->Quantidade atual:"+produto3.getQuantEstoque());
     System.out.println("\nDigite a sua opção aqui:");
 
     int qProduto = teclado.nextInt();
+
+        produto1.pedidoFeito(qProduto);
+    teclado.close();
+    System.out.println(listaDeProdutos);
     }
+    
     
 }
 
--- poo/src/main/java/com/example/atv_complementares/atv3/Produto.java
+++ poo/src/main/java/com/example/atv_complementares/atv3/Produto.java
@@ -1,9 +1,13 @@
 package com.example.atv_complementares.atv3;
 
+import java.util.Scanner;
+
 public class Produto {
     private String nome;
     private double preco;
     private int quantEstoque;
+    
+    
     public String getNome() {
         return nome;
     }
@@ -23,6 +27,26 @@ public class Produto {
         this.quantEstoque = quantEstoque;
     }
 
-    
+    //Método para comprar os itens
+    public void pedidoFeito(double pagar){
+        Scanner teclado = new Scanner(System.in);
+        
+        if (this.nome.equals(teclado)){
+            if (this.quantEstoque>0){
+            System.out.println("Digite o quanto deseja pagar(em R$):");
+            pagar = teclado.nextDouble();
+            if (this.preco>pagar){
+                System.out.println("Não é possível fazer a compra!");
+            }else{
+                this.quantEstoque -= 1;
+                System.out.println("Compra efetuada com sucesso!\nA quantidade no estoque do material atual é:");
+            }
+            }else{
+                System.out.println("Desculpa,acabou o estoque do item no momento!");
+            }
+
+        }
+        
+    }
 
 }
