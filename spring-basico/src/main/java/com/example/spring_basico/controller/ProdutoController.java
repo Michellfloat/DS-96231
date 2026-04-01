0d685e67bd0bea62d6c6bf3677db94177d80e8d4
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-31 21:41:04 -0300

    salvando o que é necessário

--- spring-basico/src/main/java/com/example/spring_basico/controller/ProdutoController.java
+++ spring-basico/src/main/java/com/example/spring_basico/controller/ProdutoController.java
@@ -9,7 +9,6 @@ import com.example.spring_basico.repository.ProdutoRepository;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
