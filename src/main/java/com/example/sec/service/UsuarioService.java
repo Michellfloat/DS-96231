9675ba01e3fc18196920a9a5a236db8aec66b724
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-04-29 06:29:55 -0300

    Salvando mudanças e aprendendo a criptografar senhas

--- src/main/java/com/example/sec/service/UsuarioService.java
+++ src/main/java/com/example/sec/service/UsuarioService.java
@@ -3,6 +3,7 @@ package com.example.sec.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.sec.dto.UsuarioRequestDTO;
@@ -13,7 +14,10 @@ import com.example.sec.repository.UsuarioRepository;
 @Service
 public class UsuarioService {
     @Autowired
-    private UsuarioRepository usuarioRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private  UsuarioRepository usuarioRepository;
 
     public List<UsuarioResponseDTO>listarUsuarios(){
         return usuarioRepository.findAll().stream().map(u -> new UsuarioResponseDTO(u.getNome(),u.getEmail())).toList();
@@ -26,7 +30,7 @@ public class UsuarioService {
         UsuarioModel novUsuario= new UsuarioModel();
         novUsuario.setNome(usuarioDTO.getNome());
         novUsuario.setEmail(usuarioDTO.getEmail());
-        novUsuario.setSenha(usuarioDTO.getSenha());
+        novUsuario.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
         
         return usuarioRepository.save(novUsuario);
     }
